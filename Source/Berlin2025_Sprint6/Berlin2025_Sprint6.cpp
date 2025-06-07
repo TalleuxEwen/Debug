6ed37a2c4dbf6b60e99d0e0ee77b6af3e1f0b82c
TalleuxEwen <91667885+TalleuxEwen@users.noreply.github.com>
2025-06-07 10:31:42 +0200

    feat(main): adding wwise integration and lights optimisations

---
+++ Source/Berlin2025_Sprint6/Berlin2025_Sprint6.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Berlin2025_Sprint6.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Berlin2025_Sprint6, "Berlin2025_Sprint6" );
